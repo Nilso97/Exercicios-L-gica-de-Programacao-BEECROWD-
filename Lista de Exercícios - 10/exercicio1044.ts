4323e45c987102b78142174f60e128d98bcf0528
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-14 17:01:58 -0300

    resolucao dos exercicios de logica de programacao

---
+++ Lista de Exercícios - 10/exercicio1044.ts
@@ -0,0 +1,9 @@
+function numbers(a: number, b: number) {
+    if (b % a === 0) {
+      console.log("São Múltiplos");
+    } else {
+      console.log("Não são Múltiplos");
+    }
+  }
+  
+  numbers(6, 24);
