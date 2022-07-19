03fbbbf5e0c55979847bcf9c104db74f0fc00ffb
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:16:31 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 07/exercicio1070.js
+++ Lista de Exercícios - 07/exercicio1070.js
@@ -1,3 +1,13 @@
+/* Exercício 1070
+Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X, um valor por linha, inclusive o X ser for o caso.
+Entrada
+
+A entrada será um valor inteiro positivo.
+Saída
+
+A saída será uma sequência de seis números ímpares.
+*/
+
 function numbers(min, max) {
     for (var x = min; x <= max; x++) {
         if (x % 2) {
