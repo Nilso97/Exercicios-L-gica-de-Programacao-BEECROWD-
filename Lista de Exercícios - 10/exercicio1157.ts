e3356abb51745f4d98c733a51f69ae0d50974500
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:24:27 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 10/exercicio1157.ts
+++ Lista de Exercícios - 10/exercicio1157.ts
@@ -1,3 +1,13 @@
+/* Exercícios 1157
+Ler um número inteiro N e calcular todos os seus divisores.
+Entrada
+
+O arquivo de entrada contém um valor inteiro.
+Saída
+
+Escreva todos os divisores positivos de N, um valor por linha.
+*/
+
 function readNumber(num: number) {
     for (let n = 0; n <= num; n++) { 
       if (num % n === 0) {
