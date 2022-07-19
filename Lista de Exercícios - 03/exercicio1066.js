17817a2435865f97e7ac7888a2d3abe3fe2bfef6
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:01:10 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 03/exercicio1066.js
+++ Lista de Exercícios - 03/exercicio1066.js
@@ -1,3 +1,13 @@
+/* Exercício 1066
+Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
+Entrada
+
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+Saída
+
+Imprima a mensagem conforme o exemplo fornecido, uma mensagem por linha, não esquecendo o final de linha após cada uma.
+*/
+
 var values = [-5, 0, -3, -4, 12];
 
 var par = values.filter(num => num % 2 === 0).length;
