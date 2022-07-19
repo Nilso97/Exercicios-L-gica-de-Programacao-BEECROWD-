77d72d039c724247d6df3056e46ca1fa332e761a
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:10:38 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 05/exercicio1080.js
+++ Lista de Exercícios - 05/exercicio1080.js
@@ -1,3 +1,13 @@
+/* Exercício 1080 - Falta uma parte... :)
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Entrada
+
+O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+Saída
+
+Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+*/
+
 // Falta a segunda parte: "posição do maior valor"
 function randomizerValues(max, min) {
     for (var num = 0; num <= max; num++) {
