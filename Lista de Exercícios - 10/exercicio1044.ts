284a2e064ebb620e0c451762b143e649770b31f0
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:23:21 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 10/exercicio1044.ts
+++ Lista de Exercícios - 10/exercicio1044.ts
@@ -1,3 +1,13 @@
+/* Exercício 1044
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+Entrada
+
+A entrada contém valores inteiros.
+Saída
+
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
 function numbers(a: number, b: number) {
     if (b % a === 0) {
       console.log("São Múltiplos");
