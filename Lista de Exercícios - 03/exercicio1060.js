69d349407a7a221d91c5d877abc89185acca10ad
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:00:08 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 03/exercicio1060.js
+++ Lista de Exercícios - 03/exercicio1060.js
@@ -1,3 +1,13 @@
+/* Exercício 1060
+Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+Entrada
+
+Neste problema extremamente simples de repetição não há entrada.
+Saída
+
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.
+*/
+
 var num = [7,-5, 6, -3.4, 4.6, 12];
 
 function verifyValues(values) {
