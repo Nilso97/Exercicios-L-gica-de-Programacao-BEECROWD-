12af88b7d31c6e468d8943ba1d10b699ebe8137a
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 14:58:12 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 02/exercicio1038.js
+++ Lista de Exercícios - 02/exercicio1038.js
@@ -1,3 +1,14 @@
+/* Exercício 1038
+Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+Entrada
+
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+Saída
+
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+*/
+
 var cachorroQuente = 4.00;
 var xSalada = 4.50;
 var xBacon = 5.00;
