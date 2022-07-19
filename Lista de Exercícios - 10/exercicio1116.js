cfc8d9270cb89960b57edbaf62a05a682029f1cd
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:25:41 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 10/exercicio1116.js
+++ Lista de Exercícios - 10/exercicio1116.js
@@ -7,7 +7,7 @@ Saída
 
 Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
 
-Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :
+Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :)
 */
 
 function calculateNumbers(x, y) {
