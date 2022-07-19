799a2f5f127dbed2c59f3afe2e2c1bbad8cd09fa
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-07-19 15:09:05 -0300

    resolucao dos exercicios de logica de programacao

--- Lista de Exercícios - 05/exercicio1078.js
+++ Lista de Exercícios - 05/exercicio1078.js
@@ -1,3 +1,14 @@
+/* Exercício 1078
+Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+Entrada
+
+A entrada contém um valor inteiro N (2 < N < 1000).
+Saída
+
+Imprima a tabuada de N, conforme o exemplo fornecido.
+*/
+
 function tabuada(value) {
     for (var num = 1; num <= 10; num++) {
         var result = num * value;
